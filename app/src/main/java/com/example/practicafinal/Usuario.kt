80c322bbe639aad496b6acc0707509016be57bd5
JImmyh091 <jaimecaballero164@gmail.com>
2025-02-18 20:51:03 +0100

    Precio usuario

--- app/src/main/java/com/example/practicafinal/Usuario.kt
+++ app/src/main/java/com/example/practicafinal/Usuario.kt
@@ -9,6 +9,7 @@ data class Usuario (
     var correo : String = "error@gmail.com",
     var contrasenia : String = "",
     var imagen : String? = null,
-    var cartas_compradas : MutableList<Carta> = mutableListOf()/*o de nombres de cartas?*/,
-    var eventos_apuntados : MutableList<Evento> = mutableListOf()/*o de nombres de eventos?*/
+    var dinero : Float = 0f,
+    var cartas_compradas : MutableList<Carta> = mutableListOf(),
+    var eventos_apuntados : MutableList<Evento> = mutableListOf()
 )
