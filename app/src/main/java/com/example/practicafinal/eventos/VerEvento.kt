49685996ad2948e20bb7c3dd0e27a37bb44d68fd
Jimmyh091 <jaimecaballero164@gmail.com>
2025-02-19 00:04:07 +0100

    Ver carta y evento

---
+++ app/src/main/java/com/example/practicafinal/eventos/VerEvento.kt
@@ -0,0 +1,21 @@
+package com.example.practicafinal.eventos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.practicafinal.R
+
+class VerEvento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ver_evento)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
