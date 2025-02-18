8ff9d59f382d44e6479e2afb9d0218f7e227f8a9
Jimmyh091 <jaimecaballero164@gmail.com>
2025-02-19 00:05:39 +0100

    Cambio de nombres

--- app/src/main/java/com/example/practicafinal/cartas/CrearCarta.kt
+++ app/src/main/java/com/example/practicafinal/cartas/CrearCarta.kt
@@ -1,4 +1,4 @@
-package com.example.practicafinal.eventos
+package com.example.practicafinal.cartas
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.practicafinal.R
 
-class crearEventos : AppCompatActivity() {
+class CrearCarta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_crear_eventos)
+        setContentView(R.layout.activity_crear_cartas)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
