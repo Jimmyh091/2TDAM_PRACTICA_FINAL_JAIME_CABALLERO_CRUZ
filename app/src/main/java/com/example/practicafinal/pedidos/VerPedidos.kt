7aed57978fe9a0bb13c384e6e26bf6be1e47fa40
Jimmyh091 <jaimecaballero164@gmail.com>
2025-02-19 19:54:26 +0100

    autor porfin T_T

--- app/src/main/java/com/example/practicafinal/pedidos/VerPedidos.kt
+++ app/src/main/java/com/example/practicafinal/pedidos/VerPedidos.kt
@@ -6,16 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.practicafinal.R
+import com.example.practicafinal.databinding.ActivityVerPedidosBinding
 
 class VerPedidos : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVerPedidosBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityVerPedidosBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ver_pedidos)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
     }
 }
