04bdb8d77b2a17754587aa880e3044aab4ebf4d4
Jimmyh091 <jaimecaballero164@gmail.com>
2025-02-19 09:27:45 +0100

    Crear y modificar carta

--- app/src/main/java/com/example/practicafinal/cartas/CrearCarta.kt
+++ app/src/main/java/com/example/practicafinal/cartas/CrearCarta.kt
@@ -11,7 +11,7 @@ class CrearCarta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_crear_cartas)
+        setContentView(R.layout.activity_crear_carta)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
