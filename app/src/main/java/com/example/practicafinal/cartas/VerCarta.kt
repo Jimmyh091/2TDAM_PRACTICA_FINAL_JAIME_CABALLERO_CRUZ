a45008447f69da68808f53e2664ae9b0efde609f
Jimmyh091 <jaimecaballero164@gmail.com>
2025-02-19 19:46:23 +0100

    ver cartas y evento

--- app/src/main/java/com/example/practicafinal/cartas/VerCarta.kt
+++ app/src/main/java/com/example/practicafinal/cartas/VerCarta.kt
@@ -6,16 +6,49 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.practicafinal.R
+import com.example.practicafinal.databinding.ActivityVerCartaBinding
 
 class VerCarta : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVerCartaBinding
+    private lateinit var idCarta : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityVerCartaBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ver_carta)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        idCarta = intent.getStringExtra("idCarta").toString()
+
+        val carta = obtenerCarta(idCarta)
+
+        binding.verCartaNombre.text = carta.nombre
+        binding.verCartaPrecio.text = carta.precio.toString()
+        binding.verCartaCategoria.text = carta.categoria
+        binding.verCartaDescripcion.text = carta.descripcion
+        binding.verCartaStock.text = carta.stock.toString()
+
+        binding.verCartaImagen.setOnClickListener {
+            /*
+            COGER IMAGEN DEL MOVIL
+            */
+        }
+
+    }
+
+    fun obtenerCarta(idCarta : String) : Carta {
+        /*
+        OBTENER CARTAS Y DEVOLVER LA QUE TIENE LA ID
+        */
+
+        return Carta()
     }
 }
--- app/src/main/java/com/example/practicafinal/eventos/VerEvento.kt
+++ app/src/main/java/com/example/practicafinal/eventos/VerEvento.kt
@@ -6,16 +6,51 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.practicafinal.R
+import com.example.practicafinal.databinding.ActivityVerEventoBinding
 
 class VerEvento : AppCompatActivity() {
+    
+    private lateinit var binding : ActivityVerEventoBinding
+    private lateinit var idEvento : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        
+        binding = ActivityVerEventoBinding.inflate(layoutInflater)
+        
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ver_evento)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        idEvento = intent.getStringExtra("idEvento").toString()
+
+        val Evento = obtenerEvento(idEvento)
+
+        binding.verEventoNombre.text = Evento.nombre
+        binding.verEventoFecha.text = Evento.fecha
+        binding.verEventoDescripcion.text = Evento.descripcion
+        binding.verEventoPrecio.text = Evento.precio.toString()
+        binding.verEventoAforo.text = Evento.aforo_max.toString()
+        // recyclar jaja
+        // binding.verEventoUsuarios.text = Evento.usuarios.toString()
+
+        binding.verEventoImagen.setOnClickListener {
+            /*
+            COGER IMAGEN DEL MOVIL
+            */
+        }
+        
+    }
+
+    fun obtenerEvento(idEvento : String) : Evento {
+        /*
+        OBTENER EVENTOS Y DEVOLVER LA QUE TIENE LA ID
+        */
+
+        return Evento()
     }
 }
