36c1d16ec36232fcbf542d4b877016e114420d63
Jimmyh091 <jaimecaballero164@gmail.com>
2025-02-19 20:53:55 +0100

    diseño de perfil actualizado

--- app/src/main/java/com/example/practicafinal/Perfil.kt
+++ app/src/main/java/com/example/practicafinal/Perfil.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.practicafinal.databinding.ActivityPerfilBinding
 
 class Perfil : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPerfilBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityPerfilBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_perfil)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
     }
 }
