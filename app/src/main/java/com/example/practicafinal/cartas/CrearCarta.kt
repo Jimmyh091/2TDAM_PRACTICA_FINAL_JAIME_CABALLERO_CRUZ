ce1ed9d51eba588bbdc2a56c9e1ee4c8a8064074
Jimmyh091 <jaimecaballero164@gmail.com>
2025-02-19 18:47:30 +0100

    arreglos anteriores cartas ¬¬

--- app/src/main/java/com/example/practicafinal/cartas/CrearCarta.kt
+++ app/src/main/java/com/example/practicafinal/cartas/CrearCarta.kt
@@ -1,21 +1,71 @@
 package com.example.practicafinal.cartas
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.practicafinal.MenuPrincipal
 import com.example.practicafinal.R
+import com.example.practicafinal.databinding.ActivityCrearCartaBinding
 
 class CrearCarta : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCrearCartaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityCrearCartaBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_crear_carta)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.crearCartaBotonCrear.setOnClickListener {
+
+            /* BD
+            COGER CARTAS
+            */
+
+            val listaCartas = listOf(Carta())
+
+            var valido = true
+
+            for (carta in listaCartas) {
+
+                if (carta.nombre == binding.crearCartaTietTitulo.text.toString()) {
+                    valido = false
+                    break
+                }
+            }
+
+            if (valido) {
+
+                val carta = Carta(
+                    binding.crearCartaTietTitulo.text.toString(),
+                    binding.crearCartaTietDescripcion.text.toString(),
+                    binding.crearCartaTietPrecio.text.toString().toFloat(),
+                    null,
+                    binding.crearCartaTietStock.text.toString().toInt(),
+                )
+
+                /* BD
+                SUBIR CARTA A LA BASE DE DATOS
+                */
+
+            }else{
+                Toast.makeText(this, "Ya existe una carta con ese nombre", Toast.LENGTH_SHORT).show()
+            }
+
+            val intent = Intent(this, MenuPrincipal::class.java)
+            startActivity(intent)
         }
     }
 }
