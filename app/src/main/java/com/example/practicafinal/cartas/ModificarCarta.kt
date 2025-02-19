6b303dbaaf2570c01fcf708b83173d68421fb7f7
Jimmyh091 <jaimecaballero164@gmail.com>
2025-02-19 19:22:31 +0100

    modificar carta y evento

--- app/src/main/java/com/example/practicafinal/cartas/ModificarCarta.kt
+++ app/src/main/java/com/example/practicafinal/cartas/ModificarCarta.kt
@@ -1,21 +1,85 @@
 package com.example.practicafinal.cartas
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.practicafinal.MenuPrincipal
 import com.example.practicafinal.R
+import com.example.practicafinal.databinding.ActivityModificarCartaBinding
 
 class ModificarCarta : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityModificarCartaBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
+        
+        binding = ActivityModificarCartaBinding.inflate(layoutInflater)
+        
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_modificar_carta)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, listOf("Blanco", "Negro", "Azul", "Rojo", "Verde", "Amarillo"))
+        binding.modificarCartaSpinnerCategoria.adapter = adapter
+
+        binding.modificarCartaImagen.setOnClickListener{
+            /*
+            COGER IMAGEN DEL MOVIL
+            */
+        }
+
+        binding.modificarCartaBotonGuardar.setOnClickListener {
+
+            /* BD
+            COGER CARTAS
+            */
+
+            val listaCartas = listOf(Carta())
+
+            var valido = false
+
+            for (carta in listaCartas) {
+
+                if (carta.nombre == binding.modificarCartaTietTitulo.text.toString()) {
+                    valido = true
+                    break
+                }
+            }
+
+            if (valido) {
+
+                val carta = Carta(
+                    binding.modificarCartaTietTitulo.text.toString(),
+                    binding.modificarCartaTietDescripcion.text.toString(),
+                    binding.modificarCartaSpinnerCategoria.selectedItem.toString(),
+                    binding.modificarCartaTietPrecio.text.toString().toFloat(),
+                    null,
+                    binding.modificarCartaTietStock.text.toString().toInt(),
+                )
+
+                /* BD
+                ACTUALIZAR CARTA A LA BASE DE DATOS
+                */
+
+                val intent = Intent(this, MenuPrincipal::class.java)
+                startActivity(intent)
+
+            }else{
+                Toast.makeText(this, "La carta no existe", Toast.LENGTH_SHORT).show()
+            }
+
+            val intent = Intent(this, MenuPrincipal::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/practicafinal/eventos/ModificarEvento.kt
+++ app/src/main/java/com/example/practicafinal/eventos/ModificarEvento.kt
@@ -1,21 +1,98 @@
 package com.example.practicafinal.eventos
 
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.practicafinal.MenuPrincipal
 import com.example.practicafinal.R
+import com.example.practicafinal.databinding.ActivityModificarEventoBinding
 
 class ModificarEvento : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityModificarEventoBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
+        
+        binding = ActivityModificarEventoBinding.inflate(layoutInflater)
+        
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_modificar_evento)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.modificarEventoFoto.setOnClickListener {
+            /*
+            COGER FOTO DEL MOVIL
+            */
+        }
+
+        binding.modificarEventoTietFecha.setOnClickListener {
+            val calendario = Calendar.getInstance()
+            val año = calendario.get(Calendar.YEAR)
+            val mes = calendario.get(Calendar.MONTH)
+            val día = calendario.get(Calendar.DAY_OF_MONTH)
+
+            val datePicker = DatePickerDialog(this, { _, year, month, dayOfMonth ->
+                val fechaSeleccionada = "$dayOfMonth/${month + 1}/$year"
+                binding.modificarEventoTietFecha.setText(fechaSeleccionada)
+            }, año, mes, día)
+
+            datePicker.show()
+        }
+
+        binding.modificarEventoBotonGuardar.setOnClickListener {
+
+            /* BD
+            COGER CARTAS
+            */
+
+            val listaEventos = listOf(Evento())
+
+            var valido = true
+
+            for (evento in listaEventos) {
+
+                if (evento.nombre == binding.modificarEventoTietNombre.text.toString() || evento.fecha == binding.modificarEventoTietFecha.text.toString()) {
+                    valido = false
+                    break
+                }
+            }
+
+            if (valido) {
+
+                val evento = Evento(
+                    binding.modificarEventoTietNombre.text.toString(),
+                    binding.modificarEventoTietDescripcion.text.toString(),
+                    binding.modificarEventoTietFecha.text.toString(),
+                    binding.modificarEventoTietPrecio.text.toString().toFloat(),
+                    binding.modificarEventoTietAforo.text.toString().toInt(),
+                )
+
+                /* BD
+                SUBIR EVENTO A LA BASE DE DATOS
+                */
+
+                val intent = Intent(this, MenuPrincipal::class.java)
+                startActivity(intent)
+
+            }else{
+                Toast.makeText(this, "Ya existe un evento con ese nombre o en esa fecha", Toast.LENGTH_LONG).show()
+            }
+
+            val intent = Intent(this, MenuPrincipal::class.java)
+            startActivity(intent)
+
+        }
+        
     }
 }
