0f65a2e98fdbc676c41dfff109090326c4a7e063
Jimmyh091 <jaimecaballero164@gmail.com>
2025-02-19 17:56:06 +0100

    login y registro

--- app/src/main/java/com/example/practicafinal/Login.kt
+++ app/src/main/java/com/example/practicafinal/Login.kt
@@ -1,20 +1,78 @@
 package com.example.practicafinal
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.practicafinal.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+
+        binding.loginBotonLogin.setOnClickListener {
+
+            val usuario = binding.loginTietUsuario.text.toString()
+            val contrasenia = binding.loginTietContrasenia.text.toString()
+
+            /* BD
+            PEDIR USUARIOS BASE DE DATOS
+            */
+            var listaUser = listOf(Usuario())
+
+            var accedido = false
+
+            for (user in listaUser){
+                if (usuario == user.nombre && contrasenia == user.contrasenia) {
+
+                    accedido = true
+
+                    actualizarShared(user)
+
+                    val intent = Intent(this, MenuPrincipal::class.java)
+                    startActivity(intent)
+
+                    break
+                }
+
+            }
+
+            if (!accedido){
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    fun actualizarShared(usuario: Usuario){
+        var usuarioSharedPreferences = getSharedPreferences("usuario", MODE_PRIVATE)
+
+        var editor = usuarioSharedPreferences.edit()
+
+        editor.putString("nombre", usuario.nombre)
+        editor.putString("contrasenia", usuario.contrasenia)
+        editor.putString("correo", usuario.correo)
+        editor.putBoolean("admin", usuario.admin)
+        editor.putFloat("dinero", usuario.dinero)
+        editor.putString("foto", usuario.imagen)
+
+        editor.apply()
     }
 }
--- app/src/main/java/com/example/practicafinal/Registro.kt
+++ app/src/main/java/com/example/practicafinal/Registro.kt
@@ -1,20 +1,96 @@
 package com.example.practicafinal
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.practicafinal.databinding.ActivityRegistroBinding
 
 class Registro : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityRegistroBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_registro)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.registroBotonRegistro.setOnClickListener {
+
+            val usuario = binding.registroTietUsuario.text.toString()
+            val correo = binding.registroTietCorreo.text.toString()
+            val contrasenia = binding.registroTietContrasenia.text.toString()
+            val contrasenia2 = binding.registroTietRepetirContrasenia.text.toString()
+
+            /* BD
+            PEDIR USUARIOS BASE DE DATOS
+            */
+            var listaUser = listOf(Usuario())
+
+            var valido = true
+
+            for (user in listaUser){
+                if (usuario == user.nombre) {
+
+                    valido = false
+                    break
+                }
+            }
+
+            if (valido){
+
+                var usuario = Usuario(
+                    admin = false,
+                    nombre = usuario,
+                    correo = correo,
+                    contrasenia = contrasenia,
+                    dinero = 0f
+                )
+
+                /* BD
+                SUBIR USUARIO A BASE DE DATOS
+                */
+
+                Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show()
+
+                actualizarShared(usuario)
+
+                val intent = Intent(this, MenuPrincipal::class.java)
+                startActivity(intent)
+
+            }else{
+                Toast.makeText(this, "El usuario ya existe", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+    }
+
+
+    fun actualizarShared(usuario: Usuario){
+        var usuarioSharedPreferences = getSharedPreferences("usuario", MODE_PRIVATE)
+
+        var editor = usuarioSharedPreferences.edit()
+
+        editor.putString("nombre", usuario.nombre)
+        editor.putString("contrasenia", usuario.contrasenia)
+        editor.putString("correo", usuario.correo)
+        editor.putBoolean("admin", usuario.admin)
+        editor.putFloat("dinero", usuario.dinero)
+        editor.putString("foto", usuario.imagen)
+
+        editor.apply()
     }
 }
