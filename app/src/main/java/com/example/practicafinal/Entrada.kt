d29e120a42d226c7851bc0b4db974c9daea13353
Jimmyh091 <jaimecaballero164@gmail.com>
2025-02-20 03:56:23 +0100

    la verdad es qe no

--- app/src/main/java/com/example/practicafinal/Entrada.kt
+++ app/src/main/java/com/example/practicafinal/Entrada.kt
@@ -1,20 +1,39 @@
 package com.example.practicafinal
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.practicafinal.databinding.ActivityEntradaBinding
 
 class Entrada : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEntradaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityEntradaBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_entrada)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.entradaBotonInicioSesion.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        binding.entradaBotonRegistro.setOnClickListener {
+            val intent = Intent(this, Registro::class.java)
+            startActivity(intent)
+        }
+
     }
 }
