5227f0ff6bb878445fda2dbd021fea5f9877cf31
Jimmyh091 <jaimecaballero164@gmail.com>
2025-02-20 04:09:11 +0100

    voy a hacerle caso a google a ver si va

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,33 +50,28 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
+
+    implementation("com.google.firebase:firebase-storage")
+
+/*
     //DataStore
     implementation ("androidx.datastore:datastore-preferences:1.1.2")
 
-    implementation (platform("com.google.firebase:firebase-bom:33.9.0"))
-
-    // Firebase Firestore
-    implementation ("com.google.firebase:firebase-firestore-ktx")
-
-    // Firebase Realtime Database (sin especificar versión)
-    implementation ("com.google.firebase:firebase-database-ktx")
-
-    // Firebase Storage (sin especificar versión)
-    implementation ("com.google.firebase:firebase-storage-ktx")
-
-/*
-
     //Firebase
+    [
     implementation ("com.google.firebase:firebase-database:20.0.2")
-    implementation ("com.google.firebase:firebase-storage:20.0.0")
-*/
     implementation ("com.google.firebase:firebase-core:20.0.0")
+    ] error
 
+    implementation ("com.google.firebase:firebase-storage:20.0.0")
     implementation ("com.google.firebase:firebase-auth:21.0.1")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     implementation ("com.google.firebase:firebase-analytics:20.0.2")
     implementation ("com.google.android.gms:play-services-auth:20.1.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.5.1")
+*/
 
     // implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
     // implementation("com.google.firebase:firebase-analytics") no lo quiero verdad
